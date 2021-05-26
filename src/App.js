4be8178ce218df85022d233649638ac45f0d374b
Trevor Page <trevor@craftycodr.com>
2021-05-26 16:57:23 -0400

    session 2 changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import TodoApp from "./Components/TodoApp/todoApp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TodoApp, { addItem } from "./Components/TodoApp/todoApp";
+import Dashboard from "./Components/Dashboard/dashboard";
+import PageNotFound from "./Components/PageNotFound/pageNotFound";
 
 function App() {
   /**
@@ -12,8 +15,23 @@ function App() {
    *        <Footer>
    *
    */
+  addItem();
 
-  return <TodoApp />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <TodoApp />;
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+        <Route path="/">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/Components/TodoApp/todoApp.js
+++ src/Components/TodoApp/todoApp.js
@@ -1,15 +1,40 @@
+import { useEffect, useState } from "react";
 import Footer from "../Footer/footer";
 import Header from "../Header/header";
 
 const TodoApp = () => {
   const firstName = "John";
 
-  
+  // private String name = "Trevor";
+  // public String getName () {return name;}
+  // public void setName (String name) { this.name = name; }
+  const [name, setName] = useState("Trevor");
+
+  useEffect(() => {
+    console.log("Hello, my name is", name);
+  });
+
   return (
     <div>
       <Header />
       <div>
         <h1>Hello World</h1>
+        <div>
+          <label
+            htmlFor="name"
+            style={{
+              marginRight: "1rem",
+            }}
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </div>
         <h3>My name is {firstName + " Doe"}</h3>
       </div>
       <Footer />
@@ -17,4 +42,9 @@ const TodoApp = () => {
   );
 };
 
+function addItem() {
+  console.log("Adding item to Todo list");
+}
+
+export { addItem };
 export default TodoApp;
